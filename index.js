5c276bc3cf4323189da98351499bcc8d2c9eeec4
yujianping <yujianping@bytedance.com>
2018-11-14 20:51:52 +0800

    提交

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ const koa = require("koa");
 const koaStatic = require("koa-static");
 const path = require("path");
 const app = new koa();
+const Router = require("koa-router");
+const router = new Router();
+
+const version = (ctx,next) => {
+    ctx.response.type = "text";
+    ctx.response.body = "<h1>111</h1>"
+}
+
+router.all("version", version);
+app.use(router.routes());
 app.use(koaStatic(path.resolve("./static/")));
 app.listen("3389");
